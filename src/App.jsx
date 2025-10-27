342248630d1fdf407790be690e6fbf0753fc9cb0
houcine1amraoui <houcine1amraoui@gmail.com>
2025-10-27 09:50:46 +0100

    commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import "./App.css";
+import groceryCartImg from "./assets/grocery-cart.png";
+
+function App() {
+  const groceryItems = [
+    { id: 1, name: "bread", quantity: 1, completed: false },
+    { id: 2, name: "meat", quantity: 1, completed: false },
+  ];
+  return (
+    <main className="App">
+      <div>
+        <div>
+          <h4 className="success">You&apos;re Done</h4>
+          <div className="header">
+            <h1>Shopping List</h1>
+            <img src={groceryCartImg} alt="" />
+            <input
+              type="text"
+              placeholder="Add an Item"
+              className="item-input"
+            />
+          </div>
+        </div>
+        <ul>
+          <li>
+            <div className="container">
+              <input
+                type="checkbox"
+                value={groceryItems[0].completed}
+                checked={groceryItems[0].completed}
+              />
+              <p>
+                {groceryItems[0].name}{" "}
+                {groceryItems[0].quantity > 1 && (
+                  <span>x{groceryItems[0].quantity}</span>
+                )}
+              </p>
+            </div>
+            <div>
+              <button className="remove-button">X</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </main>
+  );
+}
+
+export default App;
